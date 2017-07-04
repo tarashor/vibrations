72a3d16e522fcb78a1e064dd11b7c5c58899a674
Taras Goriachko <taras@frescano.se>
2017-07-05 00:37:06 +0200

    fixed some tests

--- mymodel/f1test.m
+++ mymodel/f1test.m
@@ -1,8 +1,8 @@
 
-g_a = 0.05;
-g_f = 11;
+g_a = 0.03;
+g_f = 20;
 
-R = 1;
+R = 1/0.8;
 
 L = 2;
 p = 50;
@@ -18,8 +18,8 @@ a2 = 0;
 
 ar = (pi*R+L)/(2*R) - a1/R;
 
-x=(R + a2 + g_a.*sin(g_f.*ar)).*cos(ar);
-y=(R + a2 + g_a.*sin(g_f.*ar)).*sin(ar);
+x=(R + a2 + g_a.*cos(g_f.*ar)).*cos(ar);
+y=(R + a2 + g_a.*cos(g_f.*ar)).*sin(ar);
 
 %ar_grad = ar*180/pi
 
--- mymodel/mainGVChanges.m
+++ mymodel/mainGVChanges.m
@@ -11,7 +11,8 @@ gA=0.03;
 N = 40;
 M=6;
 
-gV=[1,2,3,4,5,6,7, 8, 9, 10,12,15,20];
+gV=[200, 300, 500];
+minW = zeros(length(gV), 1);
 
 for i=1:length(gV)
   geom=[l,K, gA, gV(i)];
@@ -23,5 +24,9 @@ for i=1:length(gV)
   [vec lam] = solve(geom, layerModel, N, M, staticIndecies);
 
   ind = 1;
-  printf ("Corrugated frequency = %d, Minimum frequency = %f\n", gV(i), sqrt(lam(ind)/rho));
+  w = sqrt(lam(ind)/rho);
+  printf ("Corrugated frequency = %d, Minimum frequency = %f\n", gV(i), w);
+  minW(i) = w;
 end
+
+plot(gV, minW, 'b-', gV, minW, 'or');
