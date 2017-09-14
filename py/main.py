bb2a70b406588b80f71bbf373c3987d236605f03
taras <taras@frescano.se>
2017-09-14 15:16:39 +0200

    fixed node indexing

--- py/main.py
+++ py/main.py
@@ -45,17 +45,26 @@ class MeshNode(object):
 
 class MeshElement(object):
 	"""docstring for MeshElement"""
-	def __init__(self, left_x, right_x, top_y, bottom_y): #, top_left_index, top_right_index, bottom_right_index, bottom_left_index):
+	def __init__(self, left_x, right_x, top_y, bottom_y, top_left_index, top_right_index, bottom_right_index, bottom_left_index):
 		self.left_x = left_x
 		self.top_y = top_y
 		self.right_x = right_x
 		self.bottom_y = bottom_y
+
+		self.top_left_index = top_left_index
+		self.top_right_index = top_right_index
+		self.bottom_right_index = bottom_right_index
+		self.bottom_left_index = bottom_left_index
 		
 	def height(self):
 		return self.top_y - self.bottom_y
 
 	def width(self):
 		return self.right_x - self.left_x
+
+	def __repr__(self):
+		#return 'Element(x={}, y={})'.format(self.left_x, self.top_y)
+		return 'Element(0="{}", 1="{}", 2="{}", 3="{}")'.format(self.top_left_index, self.top_right_index, self.bottom_right_index, self.bottom_left_index)
 
 class Mesh(object):
 	"""docstring for Mesh"""
@@ -76,7 +85,11 @@ class Mesh(object):
 			for i in range(elements_height_per_layer):
 				x = 0
 				for j in range(elements_width):
-					element = MeshElement(x, x+d_x, y, y-d_y)
+					top_left_index = i*(N+1)+j
+					top_right_index =top_left_index + 1
+					bottom_left_index = top_left_index + N + 1
+					botton_right_index = bottom_left_index + 1
+					element = MeshElement(x, x+d_x, y, y-d_y, top_left_index, top_right_index, botton_right_index, bottom_left_index)
 					elements.append(element)
 					x += d_x
 				y -= d_y
@@ -87,15 +100,15 @@ class Mesh(object):
 # 	pass
 
 
-l = 2
+l = 1
 K = 0.8
-h = 0.05
+h = 0.1
 
 gA=0.03
 gV=20
 
-N = 40
-M = 6
+N = 10
+M = 2
 
 geometry = Geometry(l,K, gA, gV)
 layer = Layer(-h/2, h/2, Material.steel())
@@ -105,6 +118,7 @@ model = Model(geometry, layers, Model.SHARNIR)
 
 mesh = Mesh.generate(model.geometry.width, layers, N, M)
 print(mesh.elements)
+print(len(mesh.elements))
 
 
 # [vec lam] = solve(geom, layerModel, N, M, staticIndecies);
