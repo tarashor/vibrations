13ad26d8084bf9aeae3a37c934c0f9250b8e17f8
taras <taras@frescano.se>
2017-10-12 14:36:07 +0200

    added vector field

--- mymodel/f1test.m
+++ mymodel/f1test.m
@@ -14,7 +14,7 @@ p = 50;
 delta = L/p;
 
 a1 = 0:delta:L;
-a2 = 0;
+a2 = 0.025;
 
 ar = (pi*R+L)/(2*R) - a1/R;
 
@@ -22,8 +22,30 @@ x=(R + a2 + g_a.*cos(g_f.*ar)).*cos(ar);
 y=(R + a2 + g_a.*cos(g_f.*ar)).*sin(ar);
 
 %ar_grad = ar*180/pi
+figure
+plot(x,y, 'g');
+hold on;
+alpha1=a1(1:4:end);
+alpha2=a2(1:4:end);
 
-plot(x,y);
+q=1+alpha2./R;
+ar = (pi*R+L)/(2*R) - alpha1./R;
+w = q+g_a*K*cos(g_f*ar);
+z = g_a*g_f*K*sin(g_f*ar);
+
+
+x=(R + a2 + g_a.*cos(g_f.*ar)).*cos(ar)
+y=(R + a2 + g_a.*cos(g_f.*ar)).*sin(ar)
+
+r11=(w.*sin(ar)+z.*cos(ar))
+r12=(-w.*cos(ar)+z.*sin(ar))
+
+r21=cos(ar)
+r22=sin(ar)
+
+quiver (x,y, r11, r12, 'r')
+hold on;
+quiver (x,y, r21,r22,'b')
 
 
 %============================================
@@ -38,4 +60,12 @@ y=(R + g_a.*cos(g_f.*psi)).*sin(psi);
 %plot(x,y);
 
 
-
+ %clf;
+ [x,y] = meshgrid (1:2:20);
+x = -0.87609
+y =  0.85087
+u =  0.77983
+v =  0.60459
+ 
+ 
+ %h = quiver (x,y, u, v);
