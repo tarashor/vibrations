0dbb0fca57ecf5c4623a93dc5dc574d3740f32cb
tarashor <taras.horyachko@gmail.com>
2018-06-08 15:37:09 +0200

    show convegence

---
+++ py/notebooks/jupyter_utils.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class ListTable(list):
+    """ Overridden list class which takes a 2-dimensional list of 
+        the form [[1,2,3],[4,5,6]], and renders an HTML Table in 
+        IPython Notebook. """
+    
+    def _repr_html_(self):
+        html = ["<table>"]
+        for row in self:
+            html.append("<tr>")
+            
+            for col in row:
+                html.append("<td>{0}</td>".format(col))
+            
+            html.append("</tr>")
+        html.append("</table>")
+        return ''.join(html)
